643dbf562fdce9bc248fb50dcfc24840dcc4612d
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-09-27 18:52:58 +0200

    update

--- Node-Express-MongoDB/app/server.js
+++ Node-Express-MongoDB/app/server.js
@@ -1,9 +1,12 @@
+//requiring dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+//initialising express app
 const app = express();
 
+//setting cors option
 var corsOptions = {
     origin: 'http://localhost:8081'
 }
