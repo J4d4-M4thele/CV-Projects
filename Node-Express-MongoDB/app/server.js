a58faafd6aea0a54c7f9620261aea602e243536d
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-09-27 19:02:29 +0200

    update

--- Node-Express-MongoDB/app/server.js
+++ Node-Express-MongoDB/app/server.js
@@ -9,4 +9,21 @@ const app = express();
 //setting cors option
 var corsOptions = {
     origin: 'http://localhost:8081'
-}
+};
+
+app.use(cors(corsOptions));
+//parse content-type requests
+//in json format
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//route
+app.get('/', (req, res) => {
+  res.json({message: "Welcome to my application."})
+});
+
+//setting port and print message to terminal
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`)
+});
