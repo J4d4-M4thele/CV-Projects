099acec0a7f6372a6363d466a8ac2770a7dd95b1
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-09-27 19:30:58 +0200

    update

---
+++ Node-Express-MongoDB/app/controllers/tutorial.controller.js
@@ -0,0 +1,23 @@
+const db = require('../models');
+const Tutorial = db.tutorials;
+
+//create and save tutorial
+exports.create = (req, res) => {};
+
+//retrieve all tutorials
+exports.findAll = (req, res) => {};
+
+//find one tutorial
+exports.findOne = (req, res) => {};
+
+//update tutorial acc. to specified id
+exports.update = (req, res) => {};
+
+//delete tutorial acc. to specified id
+exports.delete = (req, res) => {};
+
+//delete all tutorials
+exports.deleteAll = (req, res) => {};
+
+//find all that are published
+exports.findAllPublished = (req, res) => {};
