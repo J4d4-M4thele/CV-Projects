8502c463bcef14fa62bb99f8cda368e9d90ef733
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-09-28 10:59:56 +0200

    update

--- Node-Express-MongoDB/react-crud/src/service/tutorial.service.js
+++ Node-Express-MongoDB/react-crud/src/service/tutorial.service.js
@@ -8,6 +8,21 @@ class TutorialDataService {
         return http.get(`/tutorials/${id}`);
     }
     create(data) {
-        return http.get('/tutorials', data);
+        return http.post('/tutorials', data);
+    }
+    update(id, data) {
+        return http.put(`/tutorials/${id}`, data);
+    }
+    delete(id) {
+        return http.delete(`/tutorials/${id}`);
+    }
+    deleteAll() {
+        return http.delete('/tutorials');
+    }
+    findByTitle(title) {
+        return http.get(`/tutorials?title=${title}`)
     }
 }
+
+const tutorialDataService = TutorialDataService()
+export default tutorialDataService;
