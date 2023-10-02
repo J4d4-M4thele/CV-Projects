4588ca6b4ee2d572919d81f20097a357e375893e
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-02 11:19:04 +0200

    update

--- Node-Express-MongoDB/react-crud/src/services/tutorial.service.js
+++ Node-Express-MongoDB/react-crud/src/services/tutorial.service.js
@@ -2,31 +2,31 @@ import http from "../http-common";
 
 class TutorialDataService {
   getAll() {
-    return http.get("/tutorials");
+    return http.get("http://localhost:8080/tutorials");
   }
 
   get(id) {
-    return http.get(`/tutorials/${id}`);
+    return http.get(`http://localhost:8080/tutorials/${id}`);
   }
 
   create(data) {
-    return http.post("/tutorials", data);
+    return http.post("http://localhost:8080/tutorials", data);
   }
 
   update(id, data) {
-    return http.put(`/tutorials/${id}`, data);
+    return http.put(`http://localhost:8080/tutorials/${id}`, data);
   }
 
   delete(id) {
-    return http.delete(`/tutorials/${id}`);
+    return http.delete(`http://localhost:8080/tutorials/${id}`);
   }
 
   deleteAll() {
-    return http.delete(`/tutorials`);
+    return http.delete(`http://localhost:8080/tutorials`);
   }
 
   findByTitle(title) {
-    return http.get(`/tutorials?title=${title}`);
+    return http.get(`http://localhost:8080/tutorials?title=${title}`);
   }
 }
 
