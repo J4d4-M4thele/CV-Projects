d3cc81d8a40ea35a2bcf4363ce7237d333aa51d5
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-02 11:56:38 +0200

    update

--- Node-Express-MongoDB/app/server.js
+++ Node-Express-MongoDB/app/server.js
@@ -1,47 +1,43 @@
-//requiring dependencies
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose'); 
+const express = require("express");
+const cors = require("cors");
 
-//initialising express app
 const app = express();
 
-//linking to database
-const db = require('./models/index');
-mongoose.set('strictQuery', true);
-db.mongoose.connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => {
-    console.log('Connected to database!');
-})
-.catch(err => {
-    console.log('Cannot connect to database!',err);
-    process.exit();
-})
-
-//setting cors option
 var corsOptions = {
-    origin: 'http://localhost:8081'
+  origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
-//parse content-type requests
-//in json format
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-//route
-app.get('/', (req, res) => {
-  res.json({message: "Welcome to bezkoder application."})
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
 });
 
-require("./routes/tutorial.routes")(app);
+require("./app/routes/turorial.routes")(app);
 
-//setting port and print message to terminal
+// set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`)
+  console.log(`Server is running on port ${PORT}.`);
 });
