002335f178badbeb0548f907bff6a8a5a8175444
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-02 10:17:26 +0200

    update

--- Node-Express-MongoDB/react-crud/src/App.js
+++ Node-Express-MongoDB/react-crud/src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Link } from "react-router-dom";
-import { Route, Routes } from "react-router-dom";
-
+import "./App.css";
 
 import AddTutorial from "./components/add-tutorial.component";
 import Tutorial from "./components/tutorial.component";
@@ -12,11 +11,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        {/* navigation bar */}
         <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <a href="/tutorials" className="navbar-brand">
+          <Link to={"/tutorials"} className="navbar-brand">
             bezKoder
-          </a>
+          </Link>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
               <Link to={"/tutorials"} className="nav-link">
@@ -30,13 +28,13 @@ class App extends Component {
             </li>
           </div>
         </nav>
-        {/* routes which link on navbar  goes to */}
+
         <div className="container mt-3">
           <Routes>
-            <Route path="/" element={<TutorialsList />} />
-            <Route path="/tutorials" element={<TutorialsList />} />
-            <Route path="/add" element={<AddTutorial />} />
-            <Route path="/tutorials/:id" element={<Tutorial />} />
+            <Route path="/" element={<TutorialsList/>} />
+            <Route path="/tutorials" element={<TutorialsList/>} />
+            <Route path="/add" element={<AddTutorial/>} />
+            <Route path="/tutorials/:id" element={<Tutorial/>} />
           </Routes>
         </div>
       </div>
