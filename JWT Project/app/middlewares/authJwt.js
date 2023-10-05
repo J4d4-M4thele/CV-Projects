55a19f56787405dd27da47f992d8f33af5a6e405
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-05 15:23:35 +0200

    update

---
+++ JWT Project/app/middlewares/authJwt.js
@@ -0,0 +1,104 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
+const db = require("../models");
+const User = db.user;
+const Role = db.role;
+
+verifyToken = (req, res, next) => {
+    let token = req.session.token;
+
+    if (!token) {
+        return res.status(403).send({ message: "No token provided!" });
+    }
+
+    //use secret to encode password
+    jwt.verify(token,
+        config.secret,
+        (err, decoded) => {
+            if (err) {
+                return res.status(401).send({
+                    message: "Unauthorized!",
+                });
+            }
+            req.userId = decoded.id;
+            next();
+        });
+};
+//finding info (id, role)
+isAdmin = (req, res, next) => {
+    User.findById(req.userId).exec((err, user) => {
+        if (err) {
+            res.status(500).send({ message: err });
+            return;
+        }
+
+        Role.find(
+            {
+                _id: { $in: user.roles },
+            },
+            (err, roles) => {
+                if (err) {
+                    res.status(500).send({ message: err });
+                    return;
+                }
+
+                for (let i = 0; i < roles.length; i++) {
+                    if (roles[i].name === "admin") {
+                        next();
+                        return;
+                    }
+                }
+
+                res.status(403).send({ message: "Require Admin Role!" });
+                return;
+            }
+        );
+    });
+};
+//seeing if the role is moderator by looking at info
+isModerator = (req, res, next) => {
+    User.findById(req.userId).exec((err, user) => {
+        if (err) {
+            res.status(500).send({ message: err });
+            return;
+        }
+
+        Role.find(
+            {
+                _id: { $in: user.roles },
+            },
+            (err, roles) => {
+                if (err) {
+                    res.status(500).send({ message: err });
+                    return;
+                }
+
+                for (let i = 0; i < roles.length; i++) {
+                    if (roles[i].name === "moderator") {
+                        next();
+                        return;
+                    }
+                }
+
+                res.status(403).send({ message: "Require Moderator Role!" });
+                return;
+            }
+        );
+    });
+};
+
+const authJwt = {
+    verifyToken,
+    isAdmin,
+    isModerator,
+};
+module.exports = authJwt;
+middlewares / index.js
+
+const authJwt = require("./authJwt");
+const verifySignUp = require("./verifySignUp");
+
+module.exports = {
+    authJwt,
+    verifySignUp
+};
